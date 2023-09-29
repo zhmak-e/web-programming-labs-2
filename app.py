cebb6d5dd13b95f8536024dfc504b7da459ee12c
Yegor Zhmak <yegor.zhmak@eltex.loc>
2023-09-29 14:08:25 +0700

    refactoring for lab2

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
+from lab2 import lab2
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
+app.register_blueprint(lab2)
 
 
 @app.route('/lab2/example')
---
+++ lab2.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, redirect, url_for, render_template
+lab2 = Blueprint('lab2', __name__)
+
+
+@lab2.route('/lab2/example')
+def example():
+    name, lab_num, group, course = 'Иван Иванов', 2, 'ФБИ-00', 3
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+    ]
+    return render_template('example.html',
+                            name=name, lab_num=lab_num, group=group,
+                            course=course, fruits=fruits)
+
+
+@lab2.route('/lab2/')
+def lab():
+    return render_template('lab2.html')
+
+
+@lab2.route('/lab2/books')
+def base():
+    return render_template('books.html')
